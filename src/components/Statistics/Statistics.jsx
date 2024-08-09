0e8130a0eb0dd5433c7ffe21f96d1f12677f917f
Serhii Tyron <mestn1y@hotmail.com>
2024-08-09 21:26:36 +0300

    task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,33 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+export const Statistics = ({ title, stats }) => {
+  const iconsArr = [
+    <FaRegThumbsUp key="0" />,
+    <MdOutlineProductionQuantityLimits key="1" />,
+    <MdPeople key="2" />,
+    <GiTreeDoor key="3" />,
+  ];
+
+  return (
+    <>
+      {title && <h2 className={style.title}>Main Statistics</h2>}
+
+      <ul className={style.list}>
+        {stats.map((stat, index) => {
+          return (
+            <StatisticsItem
+              key={stat.id}
+              title={stat.title}
+              total={stat.total}
+              icon={iconsArr[index]}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 };
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,14 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import styled from './Statistics.module.css';
+
+export const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <li className={styled.item}>
+      <IconContext.Provider value={{ size: 28, color: 'green' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={styled.counter}>{total}</span>
+      <p className={styled.text}>{title}</p>
+    </li>
+  );
 };
